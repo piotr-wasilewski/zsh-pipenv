ed5c1dbb7fa09291017d36d21add1f10bb13299c
Piotr Wasilewski <piotr.wasyl@gmail.com>
2018-06-08 10:40:47 +0200

    Update pipenv.plugin.zsh
    Autocompletion simplify even better
    Check for pipenv command

--- pipenv.plugin.zsh
+++ pipenv.plugin.zsh
@@ -1,6 +1,11 @@
 # Called by zsh when directory changes, append to chpwd_functions
 chpwd_functions+=(chpwd_checkforpipenv)
 function chpwd_checkforpipenv() {
+    if [ ! "$(command -v pipenv)" ]; then
+      echo "Pipenv not installed. See http://docs.pipenv.org/en/latest/"
+      return 1
+    fi
+  
     if [[ -a "${PWD}/Pipfile" ]]; then
         if [[ "${VIRTUAL_ENV}" != $(pipenv --venv) ]]; then
             echo -n "Pipfile exists. Do you want spawn a shell with the virtualenv? (y)"
@@ -13,12 +18,8 @@ function chpwd_checkforpipenv() {
     fi
 }
 
-#compdef pipenv
-_pipenv() {
-  eval $(env COMMANDLINE="${words[1,$CURRENT]}" _PIPENV_COMPLETE=complete-zsh  pipenv)
-}
+# Add activate to change pwd functions
+chpwd_functions+=(zsh-pipenv-shell-activate)
 
-if [[ "$(basename ${(%):-%x})" != "_pipenv" ]]; then
-  autoload -U compinit && compinit
-  compdef _pipenv pipenv
-fi
+# enable pipenv tab completion
+eval "$(pipenv --completion)"
